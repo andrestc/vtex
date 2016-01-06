43845b27e4e987aaf2f063b3919148ce7d4cf30e
André Carvalho <asantostc@gmail.com>
2016-01-06 12:47:28 -0200

    Refactors find path for tail recursion

--- src/main/scala/com/vtext/path/PathFinder.scala
+++ src/main/scala/com/vtext/path/PathFinder.scala
@@ -1,5 +1,8 @@
 package com.vtext.path
 
+import scala.annotation.tailrec
+import scala.collection.{mutable => m}
+
 case class Graph[T](paths: Map[T, List[T]]) {
   def neighbours(node: T) = paths.getOrElse(node, List.empty)
 }
@@ -7,16 +10,29 @@ case class Graph[T](paths: Map[T, List[T]]) {
 object PathFinder {
 
   def findPath[T](graph: Graph[T], start: T, end: T): Option[List[T]] = {
-    def inner(current: T, visited: List[T] = List.empty): List[T] = {
-      if(current == end) visited :+ current
-      else {
-        graph.neighbours(current).filterNot(visited.contains) match {
-          case x :: xs => (x :: xs).flatMap(n => inner(n, visited :+ current))
-          case Nil => List.empty
+
+    @tailrec
+    def inner(stack: m.Stack[(T, Int)], path: m.Stack[(T, Int)], visited: T*): Option[List[T]] = {
+      if(stack.nonEmpty) {
+        val (current, depth) = {
+          val (current, depth) = stack.pop
+          while (depth > 0 && path.head._2 >= depth) path.pop
+          path.push((current, depth))
+          (current, depth)
         }
+        if(!(current == end)){
+          graph.neighbours(current).filterNot(visited.contains).foreach(n => stack.push((n, depth+1)))
+          inner(stack, path, visited :+ current:_*)
+        } else {
+          Option(path.toList.map(_._1).reverse)
+        }
+      } else {
+        None
       }
     }
-    Option(inner(start)).filterNot(_.isEmpty)
+
+    inner(m.Stack((start, 0)), m.Stack())
+
   }
 
 }
