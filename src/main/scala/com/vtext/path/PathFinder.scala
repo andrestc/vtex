eb49aecd17c67c9caa3fd4ae05b78ef673edb3df
André Carvalho <asantostc@gmail.com>
2016-01-05 23:09:11 -0200

    Adds simple recursive solution for the path finding test

--- src/main/scala/com/vtext/path/PathFinder.scala
+++ src/main/scala/com/vtext/path/PathFinder.scala
@@ -3,7 +3,19 @@ package com.vtext.path
 object PathFinder {
 
   def findPath(paths: Map[Int, List[Int]], start: Int, end: Int): Option[List[Int]] = {
-    Option.empty
+    def inner(curr: Int, visited: List[Int] = List.empty): List[Int] = {
+      if(curr == end) visited :+ curr
+      else {
+        paths.getOrElse(curr, List.empty).filterNot(visited.contains) match {
+          case x :: xs => (x :: xs).flatMap(n => inner(n, visited :+ curr))
+          case Nil => List.empty
+        }
+      }
+    }
+    inner(start) match {
+      case x :: xs => Some(x :: xs)
+      case Nil => Option.empty
+    }
   }
 
 }
--- src/test/scala/com/vtext/path/PathFinderSpec.scala
+++ src/test/scala/com/vtext/path/PathFinderSpec.scala
@@ -4,15 +4,30 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class PathFinderSpec extends FlatSpec with Matchers {
 
-  it should "find a path" in {
-    val path = PathFinder.findPath(Map(1 -> List(2, 3), 2 -> List(4), 3 -> List(5)), 1, 4)
+  val graph = Map(1 -> List(2, 3), 2 -> List(4), 3 -> List(5))
+  val cycledGraph = Map(1 -> List(2, 3), 2 -> List(3, 4), 4 -> List(5), 6 -> List(7))
 
-    path.get should contain theSameElementsInOrderAs List(1, 2, 4)
+  it should "find a path" in {
+    PathFinder.findPath(graph, 1, 4).get should contain theSameElementsInOrderAs List(1, 2, 4)
   }
 
-  it should "return None when there is no path" in pending
+  it should "return None when there is no path" in {
+    PathFinder.findPath(graph, 4, 5) should not be defined
+  }
 
-  it should "handle cycles when there is a path" in pending
+  it should "return None when start doesn't have adjacent nodes" in {
+    PathFinder.findPath(graph, 5, 1) should not be defined
+  }
 
-  it should "handle cycles when there is no path" in pending
+  it should "return None if start node doesn't exist" in {
+    PathFinder.findPath(graph, 10, 1) should not be defined
+  }
+
+  it should "handle cycles when there is a path" in {
+    PathFinder.findPath(cycledGraph, 1, 5).get should contain theSameElementsInOrderAs List(1, 2, 4, 5)
+  }
+
+  it should "handle cycles when there is no path" in {
+    PathFinder.findPath(cycledGraph, 1, 6) should not be defined
+  }
 }
